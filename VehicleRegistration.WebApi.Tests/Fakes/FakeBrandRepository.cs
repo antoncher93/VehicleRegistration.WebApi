c4a6a8c5b4389f295a5373291a19ea83cca547e4
Anton Chernomazov <anton.chernomazov@retailrocket.io>
2023-05-31 23:40:26 +0300

    Add tests for controller

---
+++ VehicleRegistration.WebApi.Tests/Fakes/FakeBrandRepository.cs
@@ -0,0 +1,31 @@
+﻿using VehicleRegistration.WebApi.Models;
+using VehicleRegistration.WebApi.Repositories;
+
+namespace VehicleRegistration.WebApi.Tests.Fakes;
+
+public class FakeBrandRepository : IBrandRepository
+{
+    private readonly List<Brand> _brands = new();
+    
+    public Task<List<Brand>> GetBrandsAsync()
+    {
+        return Task.FromResult(_brands.ToList());
+    }
+
+    public Task<Brand?> GetBrandAsync(string brandName)
+    {
+        var result = _brands.FirstOrDefault(b => b.Name == brandName);
+        return Task.FromResult(result);
+    }
+
+    public Task AddAsync(Brand brand)
+    {
+        _brands.Add(brand);
+        return Task.CompletedTask;
+    }
+
+    public void SetupManyBrands(IEnumerable<Brand> brands)
+    {
+        _brands.AddRange(brands);
+    }
+}
---
+++ VehicleRegistration.WebApi.Tests/Fakes/FakeEngineTypeRepository.cs
@@ -0,0 +1,33 @@
+﻿using VehicleRegistration.WebApi.Models;
+using VehicleRegistration.WebApi.Repositories;
+
+namespace VehicleRegistration.WebApi.Tests.Fakes;
+
+public class FakeEngineTypeRepository : IEngineTypeRepository
+{
+    private readonly List<EngineType> _engineTypes = new();
+    
+    public Task<List<EngineType>> GetAllAsync()
+    {
+        return Task.FromResult(_engineTypes.ToList());
+    }
+
+    public Task<EngineType?> GetByIdAsync(int id)
+    {
+        var result = _engineTypes.FirstOrDefault(et => et.Id == id);
+        
+        return Task
+            .FromResult(result);
+    }
+
+    public Task AddAsync(EngineType engineType)
+    {
+        _engineTypes.Add(engineType);
+        return Task.CompletedTask;
+    }
+
+    public void SetupManyEngineTypes(IEnumerable<EngineType> engineTypes)
+    {
+        _engineTypes.AddRange(engineTypes);
+    }
+}
